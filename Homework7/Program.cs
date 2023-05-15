b4df37434d8614555295a29d7d1432c66dde4cec
RuslanHamidov <rhamidov202@gmail.com>
2023-05-15 13:22:03 +0400

    Добавьте файлы проекта.

---
+++ Homework7/Program.cs
@@ -0,0 +1,130 @@
+﻿namespace Homework7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            #region Task1
+
+            //Console.WriteLine("Enter 4 numbers: ");
+            //int a = int.Parse(Console.ReadLine());
+            //int b = int.Parse(Console.ReadLine());
+            //int c = int.Parse(Console.ReadLine());
+            //int d = int.Parse(Console.ReadLine());
+            //int sum = 0;
+
+            //int[] arr = { a, b, c, d };
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    sum = sum + arr[i];
+
+            //}
+            //Console.WriteLine($"Sum of array is {sum}");
+
+
+            #endregion
+
+
+            #region Task2
+
+            //int [] arr = {15, 54, 2, 10};
+
+            //for (int i = 0; i < arr.Length; i++)
+
+            //{
+            //    Console.WriteLine(arr[i]);
+
+            //}
+
+            // Console.WriteLine("-------------");
+
+            //Array.Sort(arr);
+            //for (int j = 0; j < arr.Length; j++)
+
+            //{
+            //    Console.WriteLine(arr[j]);
+
+            //}
+
+
+            #endregion
+
+
+            #region Task3
+            //int[] arr = {1 , 2  , 3 , 4};
+
+            //Console.WriteLine("Enter any number: ");
+
+            //int a = int.Parse(Console.ReadLine());
+
+            //for (int i = 0; i < arr.Length; i++)
+            //{
+            //    if (arr[i] == a)
+            //    {
+
+            //        Console.WriteLine($"The index of number {a} is {i}");
+            //        return;
+
+            //    }
+
+            //}
+
+            //for (int j = 0; j < arr.Length; j++)
+
+            //{
+            //    if (arr[j] != a)
+            //    {
+            //        Console.WriteLine("Error");
+            //        break;
+
+            //    }
+
+            //}
+
+
+            #endregion
+
+
+            #region Task4
+
+            //int[] arr = { 1, 2, 3, 4, 5, };
+
+            //for (int i = 1; i <= arr.Length; i++)
+            //{
+            //    Console.WriteLine(arr[^i]);
+            //}
+
+            #endregion
+
+
+            #region Task5
+
+
+            /*  
+            int [] arr = {15, 54, 2, 10};
+            int sum = 0;
+
+
+            Console.WriteLine($"The minimum value of array is " + arr.Min());
+            Console.WriteLine($"The maximum value of array is " + arr.Max());
+
+             for (int i = 0; i < arr.Length; i++)
+
+            {
+                sum += arr[i];
+
+            }
+             Console.WriteLine($"The sum of array is {sum}");
+             Console.WriteLine($"The average of array is {sum/arr.Length}");
+
+           */
+            #endregion
+
+
+
+        }
+    }
+}
